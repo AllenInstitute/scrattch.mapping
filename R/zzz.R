3fe9960f173a912d69f0687deca59467ef0011fa
Inkar Kapen <inkar.kapen@ibs-hct-ux3.corp.alleninstitute.org>
2024-07-24 15:46:14 -0700

    python set up update in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
           ## Run once more to produce an error if Docker path also fails.
           reticulate::py_set_seed(1234)
           if (py_config()$python != "/usr/bin/python3") {
-            message("Reticulate failed to detect a usable python environment. Please set the correct python path via `reticulate::use_python`.")
+            message("Reticulate failed to detect a usable python environment. Please set the correct python path via `reticulate::use_python(\"/usr/bin/python3\")`.")
           }
         }
     )
